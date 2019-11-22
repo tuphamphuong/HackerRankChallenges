090f0be701a18aeed7e8215b21d608260e8c43f0
Tu Pham <phamptu@gmail.com>
2019-11-22 16:05:12 +0700

    add algorithms warmup challenges

---
+++ src/com/tupp/algorithms/warmup/AVeryBigSum.java
@@ -0,0 +1,53 @@
+package com.tupp.algorithms.warmup;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+/**
+ * Created by Tu Pham Phuong - phamptu@gmail.com on 11/22/19.
+ */
+// https://www.hackerrank.com/challenges/a-very-big-sum/problem
+public class AVeryBigSum {
+    // Complete the aVeryBigSum function below.
+    static long aVeryBigSum(long[] ar) {
+        long result = 0;
+        for (long a: ar) {
+            result += a;
+        }
+
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long[] ar = new long[arCount];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arCount; i++) {
+            long arItem = Long.parseLong(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        long result = aVeryBigSum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
---
+++ src/com/tupp/algorithms/warmup/CompareTheTriplets.java
@@ -0,0 +1,60 @@
+package com.tupp.algorithms.warmup;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Created by Tu Pham Phuong - phamptu@gmail.com on 11/22/19.
+ */
+// https://www.hackerrank.com/challenges/compare-the-triplets/problem
+public class CompareTheTriplets {
+    // Complete the compareTriplets function below.
+    static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        List<Integer> result = new ArrayList<>();
+        Integer aliceScore = 0;
+        Integer bobScore = 0;
+
+        for (int i = 0; i< 3; i++) {
+            if (a.get(i) > b.get(i)) aliceScore ++;
+            else if (a.get(i) < b.get(i)) bobScore ++;
+        }
+        result.add(aliceScore);
+        result.add(bobScore);
+
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = compareTriplets(a, b);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
